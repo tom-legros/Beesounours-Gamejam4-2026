9ba015ad63c02319173f16e55898423aa3116226
Tom <tom.legros@etudiant.univ-reims.fr>
2026-01-09 19:31:27 +0100

    Ajout de la fonctionnalité de Sprint

--- game/player.gd
+++ game/player.gd
@@ -1,11 +1,20 @@
 extends CharacterBody2D
 
-const SPEED = 100
+const WALK_SPEED = 150.0  # Vitesse de marche normale
+const SPRINT_SPEED = 350.0 # Vitesse quand on sprint
 
-func _physics_process(_delta: float):
+func _physics_process(_delta):
+	
+	var current_speed = WALK_SPEED
+
+	if Input.is_key_pressed(KEY_SHIFT):
+		current_speed = SPRINT_SPEED
+
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
 	if direction != Vector2.ZERO:
-		velocity = direction * SPEED
+		velocity = direction * current_speed
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+		velocity = velocity.move_toward(Vector2.ZERO, current_speed)
+
 	move_and_slide()
