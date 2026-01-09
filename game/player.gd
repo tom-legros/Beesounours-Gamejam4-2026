f9e93cd0ac5c49d05c2b2db9f93846a675382809
Tom <tom.legros@etudiant.univ-reims.fr>
2026-01-09 19:03:56 +0100

    Ajout des mouvements du Player (Fonction _physics_process())

--- game/player.gd
+++ game/player.gd
@@ -1,25 +1,11 @@
 extends CharacterBody2D
 
+const SPEED = 100
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func _physics_process(_delta: float):
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if direction != Vector2.ZERO:
+		velocity = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
 	move_and_slide()
