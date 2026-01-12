42d8d2c9f003a94b412c162db64482b98893a78e
Tom <tom.legros@etudiant.univ-reims.fr>
2026-01-12 18:42:36 +0100

    Mise a jour de l'IA des renards

--- game/renard.gd
+++ game/renard.gd
@@ -1,22 +1,47 @@
 extends CharacterBody2D
 
-var vitesse = 100
-var cible = null
+@export var VITESSE_MAX = 80
+@export var ACCELERATION = 400
+@export var VITESSE_ERRANCE = 30
 
-func _physics_process(_delta):
+var cible = null
+var direction_errance = Vector2.ZERO
+var temps_errance = 0.0
+
+func _ready():
+	choisir_direction_aleatoire()
+
+func _physics_process(delta):
+	var direction_voulue = Vector2.ZERO
+
 	if cible != null:
-		var direction = global_position.direction_to(cible.global_position)
-		velocity = direction * vitesse
-		if direction.x > 0:
-			$Sprite2D.flip_h = true
-		elif direction.x < 0:
-			$Sprite2D.flip_h = false
-		move_and_slide()
-		
+		direction_voulue = global_position.direction_to(cible.global_position)
+	else:
+		temps_errance -= delta
+		if temps_errance <= 0:
+			choisir_direction_aleatoire()
+		direction_voulue = direction_errance
+
+	velocity = velocity.move_toward(direction_voulue * (VITESSE_MAX if cible else VITESSE_ERRANCE), ACCELERATION * delta)
+
+	if velocity.x > 2: 
+		$Sprite2D.flip_h = true
+	elif velocity.x < -2:
+		$Sprite2D.flip_h = false
+	
+	move_and_slide()
+
+func choisir_direction_aleatoire():
+	var angle = randf_range(0, 2 * PI)
+	direction_errance = Vector2(cos(angle), sin(angle))
+	temps_errance = randf_range(1.5, 4.0)
+
 func _on_vision_body_entered(body):
 	if body.name == "Player": 
 		cible = body
+		print("Le renard vous a repéré !")
 
 func _on_vision_body_exited(body):
 	if body == cible:
 		cible = null
+		choisir_direction_aleatoire()
