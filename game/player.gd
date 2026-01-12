cbffdd25677b6b7ec554d33d8ec2c1a4874cc81d
Tom <tom.legros@etudiant.univ-reims.fr>
2026-01-12 18:00:52 +0100

    Correction de la direction de l'attaque

--- game/player.gd
+++ game/player.gd
@@ -9,10 +9,11 @@ const ZOOM_SPEED = 5.0
 
 var is_attacking: bool = false
 
+
+@onready var zone_attaque_node = $ZoneAttaque 
 @onready var collision_attaque = $ZoneAttaque/CollisionShape2D
 @onready var sprite_ours = $PlayerSprite
 @onready var camera = $Camera2D
-@onready var col_attaque = $ZoneAttaque/CollisionShape2D
 @onready var animated_sprite = $PlayerSprite
 
 
@@ -24,26 +25,34 @@ var texture_avant = preload("res://img/Ours_avant.png")
 func _physics_process(_delta):
 	if Input.is_key_pressed(KEY_SPACE) and is_attacking == false:
 		lancer_attaque()
-
 	var current_speed = WALK_SPEED
 	var target_zoom = ZOOM_NORMAL 
-
 	if Input.is_key_pressed(KEY_SHIFT):
 		current_speed = SPRINT_SPEED
 		target_zoom = ZOOM_RUN    
 	if camera:
 		camera.zoom = camera.zoom.lerp(target_zoom, ZOOM_SPEED * _delta)
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	if direction.x < 0:
-		animated_sprite.flip_h = true
+	if direction.x != 0:
 		animated_sprite.play("walking")
-	elif direction.x > 0:
-		animated_sprite.flip_h = false
+		if direction.x > 0:
+			animated_sprite.flip_h = false
+			zone_attaque_node.rotation_degrees = 0  
+		else:
+			animated_sprite.flip_h = true
+			zone_attaque_node.rotation_degrees = 180 
+	elif direction.y != 0:
+		animated_sprite.flip_h = false 
+		if direction.y > 0:
+			animated_sprite.play("walking")
+			zone_attaque_node.rotation_degrees = 90  
+		else: 
+			animated_sprite.play("arriere")         
+			zone_attaque_node.rotation_degrees = -90 
 	if direction != Vector2.ZERO:
 		velocity = direction * current_speed
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, current_speed)
-
 	move_and_slide()
 
 func lancer_attaque():
