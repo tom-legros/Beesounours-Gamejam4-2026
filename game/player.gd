8b5e0fdf9de5074cf19ea5fa0ac7b5f0a518c80a
Reshomy <benjaminpierret7@gmail.com>
2026-01-12 09:09:38 +0100

    ajout d'animations, bug de direction avec sprite anime

--- game/player.gd
+++ game/player.gd
@@ -10,11 +10,13 @@ const ZOOM_SPEED = 5.0
 var is_attacking: bool = false
 
 @onready var collision_attaque = $ZoneAttaque/CollisionShape2D
-@onready var sprite_ours = $PlayerSprite
+@onready var animated_sprite = $PlayerSprite
 @onready var camera = $Camera2D 
 
-var texture_normale = preload("res://img/Ours.png")
+var texture_normale = preload("res://img/Ours_Walking1.png")
 var texture_attaque = preload("res://img/Ours_attaque.png")
+var texture_arriere = preload("res://img/Ours_arriere1.png")
+var texture_avant = preload("res://img/Ours_avant.png")
 
 func _physics_process(_delta):
 	if Input.is_key_pressed(KEY_SPACE) and is_attacking == false:
@@ -30,10 +32,16 @@ func _physics_process(_delta):
 		camera.zoom = camera.zoom.lerp(target_zoom, ZOOM_SPEED * _delta)
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if direction.x < 0:
-		sprite_ours.flip_h = true
+		animated_sprite.flip_h = true
+		animated_sprite.play("walking")
 	elif direction.x > 0:
-		sprite_ours.flip_h = false
+		animated_sprite.flip_h = false
+	elif direction.y < 0:
+		animated_sprite.play("arriere")
+	elif direction.y > 0:
+		animated_sprite.flip_h = false
 	if direction != Vector2.ZERO:
+		animated_sprite.play("arriere")
 		velocity = direction * current_speed
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, current_speed)
@@ -42,13 +50,13 @@ func _physics_process(_delta):
 
 func lancer_attaque():
 	is_attacking = true
-	if sprite_ours:
-		sprite_ours.texture = texture_attaque
+	if animated_sprite:
+		animated_sprite.texture = texture_attaque
 	collision_attaque.disabled = false
 	await get_tree().create_timer(0.4).timeout
 	collision_attaque.disabled = true
-	if sprite_ours:
-		sprite_ours.texture = texture_normale
+	if animated_sprite:
+		animated_sprite.texture = texture_normale
 	is_attacking = false
 
 func _on_zone_attaque_body_entered(body):
