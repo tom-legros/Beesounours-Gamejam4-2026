0f06cc318502a7fadf64fea25334d03d9130fcdf
Tom <tom.legros@etudiant.univ-reims.fr>
2026-01-12 18:09:27 +0100

    Correction de l'attaque

--- game/renard.gd
+++ game/renard.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var vitesse = 60
+var vitesse = 100
 var cible = null
 
 func _physics_process(_delta):
@@ -16,9 +16,7 @@ func _physics_process(_delta):
 func _on_vision_body_entered(body):
 	if body.name == "Player": 
 		cible = body
-		print("Je te vois !")
 
 func _on_vision_body_exited(body):
 	if body == cible:
 		cible = null
-		print("Il est parti...")
