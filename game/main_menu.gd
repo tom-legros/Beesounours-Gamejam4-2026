09bccc7c6d4c561fafb80b3e61959aeb4bcec4af
pier0290 <benjamin.pierret@etudiant.univ-reims.fr>
2026-01-15 01:46:39 +0100

    menu

--- game/main_menu.gd
+++ game/main_menu.gd
@@ -6,6 +6,11 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit() # Replace with function body.
